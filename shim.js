9f61e673d869210c653be1979a0333ef38a55963
baked <tristancilltseinleben@gmail.com>
2022-09-09 21:21:03 +0200

    fix broken version

--- shim.js
+++
@@ -1,6 +0,0 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-import { fileURLToPath } from "url";
-import { dirname as getDirname } from "path";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = getDirname(__filename);
--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,14 @@ export default defineConfig((options) => {
   if (!(options.entry instanceof Array)) return {};
   if (options.entry[0] === "src/cli.ts")
     return {
-      inject: ["./shim.js"],
+      banner: {
+        js: `
+import { createRequire } from 'module';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const require = createRequire(import.meta.url);
+        `,
+      },
     };
   else if (options.entry[0] === "index.ts")
     return {
