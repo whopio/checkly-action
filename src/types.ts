56764a16cc41be9c7332a9ed1d8a47f331046e24
baked <tristancilltseinleben@gmail.com>
2022-09-15 05:21:40 +0200

    added basic test runner

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface ChecklyConfig {
+  locations: string[];
+  runtimeId: string;
+  frequency: number;
+  on: "chromium" | "firefox" | "webkit";
+}
+
+export type FullChecklyConfig = ChecklyConfig & {
+  activated: boolean;
+  muted: boolean;
+  shouldFail: boolean;
+  doubleCheck: boolean;
+};
